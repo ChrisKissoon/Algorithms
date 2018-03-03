bb46386d5f2ba9ef9fe57783dd554d65a902f63d
Chris Kissoon <chriskissoon6@gmail.com>
2018-03-03 17:44:23 -0400

    Reverse String Algorthm
    This algorthm uses the three methods Split(), Reverse() and Join().

---
+++ reverseString.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <script src="game.js"></script>
+    </head>
+</html>
+
+
+
+
+function reverseStr(str){
+    
+   return str.split("").reverse("").join("");
+    
+    
+}
+
+console.log(reverseStr("Chris"));
